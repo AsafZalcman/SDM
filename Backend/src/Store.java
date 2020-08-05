31f367599e4270fd0f067732f45ca5c990ad49f1
Lior_Shmueli <shmueli@gmail.com>
2020-08-05 13:17:56 +0300

    implement getStoreIDNamePPK method for issue #2

--- Backend/src/Store.java
+++ Backend/src/Store.java
@@ -12,4 +12,27 @@ public class Store {
     private List<Order> m_StoreOrders;
     private double m_TotalDeliveriesPrice;
 
+    public int getM_StoreID() {
+        return m_StoreID;
+    }
+
+    public String getM_StoreName() {
+        return m_StoreName;
+    }
+
+    public double getM_PPK() {
+        return m_PPK;
+    }
+
+    public void setM_StoreID(int m_StoreID) {
+        this.m_StoreID = m_StoreID;
+    }
+
+    public void setM_StoreName(String m_StoreName) {
+        this.m_StoreName = m_StoreName;
+    }
+
+    public void setM_PPK(double m_PPK) {
+        this.m_PPK = m_PPK;
+    }
 }
--- Backend/src/SuperDuperManager.java
+++ Backend/src/SuperDuperManager.java
@@ -1,7 +1,32 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SuperDuperManager {
-    private Map<Integer, Store> m_StoresID2Store;
+    private Map<Integer, Store> m_StoreID2Store;
     private Map<Integer, StorageOrder> m_OrderID2Order;
     private Map<Integer, Item> m_ItemID2Item;
+
+    public SuperDuperManager() {
+        m_StoreID2Store = new HashMap<>();
+        m_OrderID2Order = new HashMap<>();
+        m_ItemID2Item = new HashMap<>();
+    }
+
+    public List<ArrayList<Object>> getStoreIDNamePPK(){
+        List<ArrayList<Object>> storeDetails = new ArrayList<>();
+        for(Map.Entry<Integer, Store> entry: m_StoreID2Store.entrySet()){
+            ArrayList<Object> store = new ArrayList<>();
+            store.add(entry.getKey());
+            store.add((entry.getValue().getM_StoreName()));
+            store.add(entry.getValue().getM_PPK());
+            storeDetails.add(store);
+        }
+        return storeDetails;
+    }
+    public void addNewStore(Store i_newStore){
+        m_StoreID2Store.put(i_newStore.getM_StoreID(), i_newStore);
+    }
 }
