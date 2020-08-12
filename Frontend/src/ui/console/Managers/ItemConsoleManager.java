f396550ed7e24ab27a060064899adfcfeea87699
Asaf <asafzalcman@gmail.com>
2020-08-12 20:00:37 +0300

    implement order history

--- Frontend/src/ui/console/Managers/ItemConsoleManager.java
+++ Frontend/src/ui/console/Managers/ItemConsoleManager.java
@@ -2,42 +2,44 @@ package ui.console.Managers;
 
 import DtoModel.ItemDto;
 import ViewModel.ItemViewModel;
-
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ItemConsoleManager {
 
     ItemViewModel m_ItemViewModel = new ItemViewModel();
 
- //  public String getAllItems()
- //  {
- //      StringBuilder res = new StringBuilder();
- //      int counter =1;
- //      for (ItemDto itemDto: m_ItemViewModel.getAllItems()
- //      ) {
- //          res.append(counter + ". " + getBasicDataFromItem(itemDto)+"\n");
- //          counter++;
- //      }
- //      return res.toString();
- //  }
+    public String getAllItems()
+    {
+        StringBuilder res = new StringBuilder();
+        int counter =1;
+        for (ItemDto itemDto: m_ItemViewModel.getAllItems()
+        ) {
+            res.append(counter + ". " + getBasicDataFromItem(itemDto));
+            counter++;
+        }
+        return res.toString();
+    }
 
     //bad name
-    public String getAllItems(int i_StoreId) {
+    public String getAllItemsWithPrice(int i_StoreId) {
         Collection<ItemDto> storeItems = m_ItemViewModel.getAllItemsOfStore(i_StoreId);
         Collection<ItemDto> allItems = m_ItemViewModel.getAllItems();
         StringBuilder res = new StringBuilder();
+        Map<Integer,ItemDto> idToItemDtoMap= new HashMap<>(storeItems.size());
         int counter = 1;
         for (ItemDto itemDto : storeItems
         ) {
-            res.append(counter).append(". ").append(getBasicDataFromItem(itemDto)).append("\n");
+            res.append(counter).append(". ").append(getBasicDataFromItem(itemDto)).append("\n").append(getPriceField(itemDto));
             counter++;
+            idToItemDtoMap.put(itemDto.getId(),itemDto);
         }
 
         for (ItemDto itemDto : allItems
         ) {
-            if (!storeItems.contains(itemDto)) {
-                res.append(counter).append(". ").append(getBasicDataFromItem(itemDto)).append("\n");
+            if (idToItemDtoMap.get(itemDto.getId())!=null) {
+                res.append(counter).append(". ").append(getBasicDataFromItem(itemDto)).append("\n").append(getPriceField(itemDto));
                 counter++;
             }
         }
@@ -46,7 +48,11 @@ public class ItemConsoleManager {
 
     public String getBasicDataFromItem(ItemDto i_ItemDto)
     {
-        String priceStr=i_ItemDto.getPrice() !=null? " Price:"+ i_ItemDto.getPrice() :" Price: not for sell";
-        return "Id:" +i_ItemDto.getId() + " Name:" + i_ItemDto.getItemName() + " Purchase from:" + i_ItemDto.getPurchaseForm() + priceStr;
+        return "Id:" +i_ItemDto.getId() + "\nName:" + i_ItemDto.getItemName() + "\nPurchase from:" + i_ItemDto.getPurchaseForm();
+    }
+
+    public String getPriceField(ItemDto i_ItemDto)
+    {
+        return i_ItemDto.getPrice() !=null? "Price:"+ i_ItemDto.getPrice() :" Price: not for sell";
     }
 }
