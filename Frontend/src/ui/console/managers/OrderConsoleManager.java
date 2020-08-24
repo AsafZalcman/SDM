e07f7981ad10de6f49b8178404cdec813e4a0d9b
AsafZalcman <50069403+AsafZalcman@users.noreply.github.com>
2020-08-24 15:46:24 +0300

    Revert "Merge commit."

--- Frontend/src/ui/console/managers/OrderConsoleManager.java
+++ Frontend/src/ui/console/managers/OrderConsoleManager.java
@@ -14,6 +14,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+
 import static ui.console.utils.FormatUtils.messageFormat;
 
 public class OrderConsoleManager {
--- Frontend/src/ui/console/managers/StoreConsoleManager.java
+++ Frontend/src/ui/console/managers/StoreConsoleManager.java
@@ -11,78 +11,51 @@ import ui.console.utils.*;
 import java.util.Collection;
 import java.util.Scanner;
 
-import static ui.console.utils.FormatUtils.messageFormat;
-
 public class StoreConsoleManager {
     private StoreViewModel m_StoreViewModel = new StoreViewModel();
     private ItemViewModel m_ItemViewModel = new ItemViewModel();
 
-
-    public int getStoreIdFromUser()
-    {
-        Collection<StoreDto> stores = m_StoreViewModel.getAllStores();
-        int counter = 1;
-        StringBuilder storesDetails = new StringBuilder();
-        storesDetails.append("Stores:").append("\n");
-        for (StoreDto storeDto:stores) {
-            storesDetails.append(counter).append(".\n")
-                    .append(UniquelyUtil.getIdString(storeDto.getId())).append("\n")
-                    .append(UniquelyUtil.getNameString(storeDto.getName())).append("\n")
-                    .append(StoreDtoUtil.getPricePerKmString(storeDto.getPPK())).append("\n");
-            counter++;
-        }
-        return getStoreIDFromUser(storesDetails.toString());
-    }
-
-    private int getStoreIDFromUser(String i_StoreDetails){
+    public int getStoreIdFromUser() {
         Scanner scanner = new Scanner(System.in);
         String userChoiceStr = null;
         int userChoice;
 
-        System.out.println(i_StoreDetails);
+        Collection<StoreDto> stores = m_StoreViewModel.getAllStores();
+        int counter = 1;
 
+        for (StoreDto storeDto : stores
+        ) {
+            System.out.println(counter + ".\n" +
+                    UniquelyUtil.getIdString(storeDto.getId()) + "\n"
+                    + UniquelyUtil.getNameString(storeDto.getName()) + "\n"
+                    + StoreDtoUtil.getPricePerKmString(storeDto.getPPK()));
+            counter++;
+        }
         while (true) {
-            System.out.println("Please enter the Id of the desired store:");
+            System.out.println("Please enter the Id of the desired store");
             try {
                 userChoiceStr = scanner.nextLine();
                 userChoice = Integer.parseInt(userChoiceStr);
-            }
-            catch (NumberFormatException e)
-            {
-                System.out.println(messageFormat("Error: \"" + userChoiceStr +"\" is not number"));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: \"" + userChoiceStr + "\" is not number");
                 continue;
             }
-            if(!m_StoreViewModel.isStoreIDExistInTheSystem(userChoice)){
-                System.out.println(messageFormat("Error: store ID: \"" + userChoice +"\" is not available in the system"));
+            if (!m_StoreViewModel.isStoreIDExistInTheSystem(userChoice)) {
+                System.out.println("Error: store ID: \"" + userChoice + "\" is not available in the system");
                 continue;
             }
             return userChoice;
         }
     }
 
-    public int getStoreIdBasicDetailsFromUser(){
-        Collection<StoreDto> stores = m_StoreViewModel.getAllStores();
-        int counter=1;
-
-        StringBuilder storesDetails = new StringBuilder();
-        storesDetails.append("Stores:").append("\n");
-        for (StoreDto storeDto:stores) {
-            storesDetails.append(counter).append(".\n")
-                    .append(UniquelyUtil.getIdString(storeDto.getId())).append("\n")
-                    .append(UniquelyUtil.getNameString(storeDto.getName())).append("\n");
-            counter++;
-        }
-        return getStoreIDFromUser(storesDetails.toString());
-    }
-    public void ShowAllStores()
-    {
-        int counter =1;
-        System.out.println("\n" + "Stores details:");
+    public void ShowAllStores() {
+        int counter = 1;
+        System.out.println("Stores details:");
         StringBuilder storeDetails = new StringBuilder();
         for (StoreDto storeDto : m_StoreViewModel.getAllStores()
         ) {
             storeDetails.append(counter)
-                    .append(")\n")
+                    .append(".\n")
                     .append(UniquelyUtil.getIdString(storeDto.getId()))
                     .append("\n")
                     .append(UniquelyUtil.getNameString(storeDto.getName()))
@@ -90,13 +63,13 @@ public class StoreConsoleManager {
                     .append("- Items:\n")
                     .append(getAllItemsOfStore(storeDto))
                     .append("\n")
-                    .append("- Orders details:\n")
+                    .append("- Orders form the store:\n")
                     .append(getOrdersOfStore(storeDto))
                     .append("\n")
                     .append(StoreDtoUtil.getPricePerKmString(storeDto.getPPK()))
                     .append("\n")
                     .append(StoreDtoUtil.getIncomesFromDeliveriesString(storeDto.getDeliveriesIncomes()))
-                    .append("\n\n");
+                    .append("\n");
             counter++;
         }
 
@@ -147,23 +120,22 @@ public class StoreConsoleManager {
         return itemsDetails.toString();
     }
 
-    public void updateStoreItemsMenu(){
-        int storeID = getStoreIdBasicDetailsFromUser();
-        UpdateStoreMenu i_UpdateStoreMenu = new UpdateStoreMenu(
+    public void updateStoreItemsMenu() {
+        int storeID = getStoreIdFromUser();
+        UpdateStoreMenu m_UpdateStoreMenu = new UpdateStoreMenu(
                 new MenuItem("Delete item", () -> deleteItemFromStore(storeID)),
                 new MenuItem("Insert new item", () -> insertNewItemToStore(storeID)),
                 new MenuItem("Change item price", () -> changeStoreItemPrice(storeID)),
-                new MenuItem("Back to Main Menu", () -> System.out.println("All updates were saved!\n")));
-        i_UpdateStoreMenu.run();
+                new MenuItem("Back to Main Menu", () -> System.out.println("All updates were saved!!!!")));
+        m_UpdateStoreMenu.run();
     }
 
     private void deleteItemFromStore(int i_StoreID) {
         int itemID = getStoreItemIDFromUser(i_StoreID);
         try {
             m_ItemViewModel.deleteItemFromStore(i_StoreID, itemID);
-            System.out.println(messageFormat("Item ID: " + itemID + " deleted Successfully!"));
-        }catch (Exception e){
-            System.out.println(messageFormat(e.getMessage()));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -172,41 +144,36 @@ public class StoreConsoleManager {
         double newItemPrice = getPriceForNewItemFromUser();
         try {
             m_ItemViewModel.addNewItemToStore(i_StoreID, newItemID, newItemPrice);
-            System.out.println(messageFormat("Item ID: " + newItemID + " with the price: " + newItemPrice + " was Successfully added to the requested store"));
-        }catch (Exception e){
-            System.out.println(messageFormat(e.getMessage()));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
     private double getPriceForNewItemFromUser() {
-        String messageStr = "Enter price for the new item:";
-        return getPriceFromUser(messageStr);
+        System.out.println("Enter price for the new item:");
+        return getPriceFromUser();
     }
 
 
     private double getNewPriceFromUser() {
-        String messageStr = "Enter the new price:";
-        return getPriceFromUser(messageStr);
+        System.out.println("Enter the new price:");
+        return getPriceFromUser();
     }
 
-    private double getPriceFromUser(String i_MsgStr)
-        {
+    private double getPriceFromUser() {
         Scanner scanner = new Scanner(System.in);
         String userChoiceStr = null;
         double userChoice;
 
         while (true) {
-            System.out.println(i_MsgStr);
             try {
                 userChoiceStr = scanner.nextLine();
                 userChoice = Double.parseDouble(userChoiceStr);
                 if (userChoice < 0) {
                     throw new NumberFormatException();
                 }
-            }
-            catch (NumberFormatException e)
-            {
-                System.out.println(messageFormat("Error:\"" + userChoiceStr +"\" is not a positive number"));
+            } catch (NumberFormatException e) {
+                System.out.println("Error:\"" + userChoiceStr + "\" is not number");
                 continue;
             }
             return userChoice;
@@ -233,7 +200,7 @@ public class StoreConsoleManager {
                     .append("\n");
             counter++;
         }
-        System.out.println("Items:");
+
         System.out.println(res.toString());
 
         while (true) {
@@ -241,14 +208,12 @@ public class StoreConsoleManager {
                 System.out.println("Please enter the Id of the desired item:");
                 userChoiceStr = scanner.nextLine();
                 userChoice = Integer.parseInt(userChoiceStr);
-            }
-            catch (NumberFormatException e)
-            {
-                System.out.println(messageFormat("Error: \"" + userChoiceStr +"\" is not number"));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: \"" + userChoiceStr + "\" is not number");
                 continue;
             }
-            if(!m_ItemViewModel.isItemExistInTheSystem(userChoice)){
-                System.out.println(messageFormat("Error: item ID: \"" + userChoice +"\" is not available in the system"));
+            if (!m_ItemViewModel.isItemExistInTheSystem(userChoice)) {
+                System.out.println("Error: item ID: \"" + userChoice + "\" is not available in the system");
                 continue;
             }
             return userChoice;
@@ -259,7 +224,6 @@ public class StoreConsoleManager {
         int storeItemID = getStoreItemIDFromUser(i_StoreID);
         double newPrice = getNewPriceFromUser();
         m_StoreViewModel.updateStoreItemPrice(i_StoreID, storeItemID, newPrice);
-        System.out.println(messageFormat("The price for item ID: " + storeItemID + " has been successfully updates to " + newPrice));
     }
 
     private int getStoreItemIDFromUser(int i_StoreID) {
@@ -282,7 +246,7 @@ public class StoreConsoleManager {
                     .append("\n");
             counter++;
         }
-        System.out.println("Items:");
+
         System.out.println(res.toString());
 
         while (true) {
@@ -290,14 +254,12 @@ public class StoreConsoleManager {
             try {
                 userChoiceStr = scanner.nextLine();
                 userChoice = Integer.parseInt(userChoiceStr);
-            }
-            catch (NumberFormatException e)
-            {
-                System.out.println(messageFormat("Error: \"" + userChoiceStr +"\" is not number"));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: \"" + userChoiceStr + "\" is not number");
                 continue;
             }
-            if(!m_ItemViewModel.isStoreItemIDBelongToTheStore(i_StoreID, userChoice)){
-                System.out.println(messageFormat("Error: item ID: \"" + userChoice +"\" does not belong to the store ID: \"" + i_StoreID + "\""));
+            if (!m_ItemViewModel.isStoreItemIDBelongToTheStore(i_StoreID, userChoice)) {
+                System.out.println("Error: item ID: \"" + userChoice + "\" does not belong to the store ID: \"" + i_StoreID);
                 continue;
             }
             return userChoice;
